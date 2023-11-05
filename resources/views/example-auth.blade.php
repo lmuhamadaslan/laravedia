280573f2c167cd225c31667afe6e0c12ba8e9bc6
Aslan <aslan@Aslan-MBA.local>
2023-11-06 00:56:46 +0800

    initial commit

---
+++ resources/views/example-auth.blade.php
@@ -0,0 +1,6 @@
+@extends('back.layouts.auth-layout')
+@section('pageTitle', isset($pageTitle) ? $pageTitle : 'Page Title')
+
+@section('content')
+    content here...
+@endsection
---
+++ resources/views/example-page.blade.php
@@ -0,0 +1,6 @@
+@extends('back.layouts.pages-layout')
+@section('pageTitle', isset($pageTitle) ? $pageTitle : 'Page Title')
+
+@section('content')
+    content here ...
+@endsection
